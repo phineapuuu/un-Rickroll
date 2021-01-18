787940e8b9a8a2b49c71dbd4eb96ba91f323e424
Eggnog <75511393+phineapuuu@users.noreply.github.com>
2021-01-18 21:43:02 +0800

    Remove console debgging messages, change innerHTML to createTextNode

---
+++ data/detector.js
@@ -0,0 +1,9 @@
+var l = document.links;
+var rickroll = "dQw4w9WgXcQ";
+
+for(var i=0; i<l.length; i++) {
+  if(l[i].href.indexOf(rickroll) > -1) {
+    var text = document.createTextNode(" (Rickroll detected)");
+    l[i].appendChild(text);
+  }
+}
